a4be86c62d5e6c15662c1ddab87884cec380f9a3
Kavita Sahu <42509134+kvsahu@users.noreply.github.com>
2021-12-21 22:14:37 +0530

    Add files via upload

---
+++ Basic Banking System/index.php
@@ -0,0 +1,32 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+  
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Basic Banking System</title>
+  </head>
+
+  <body style="background-color: black;">
+  <header>
+  <?php
+  include 'navbar.php';
+  ?></header>
+<main>
+  <style>
+  body{
+    background-image: url('img/background.png');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center  ;
+    width: 100%;
+    
+  }
+</style>
+
+    </main>
+  </body>
+</html>
